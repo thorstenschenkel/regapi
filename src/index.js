3860295c392de22e32990532716bd273ef524c34
thorstenschenkel <myGitHub4DVAG>
2018-12-13 16:34:13 +0100

    sort of events added

--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@ const DB_COLLECTION = 'snapshots';
 const DB_PWD = process.env.DB_PWD; // 'Byd0RYnRUq1S9Nkp';
 const DB_URI = 'mongodb://snowinfo:' + DB_PWD + '@cluster0-shard-00-00-bavvq.mongodb.net:27017,cluster0-shard-00-01-bavvq.mongodb.net:27017,cluster0-shard-00-02-bavvq.mongodb.net:27017/snowinfo?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
 
+function compareEvents(e1, e2) {
+    const pattern = /(\d{2})\.(\d{2})\.(\d{4})/;
+    const date1 = new Date(e1.dateStrg.replace(pattern,'$3-$2-$1'));
+    const date2 = new Date(e2.dateStrg.replace(pattern,'$3-$2-$1'));
+    return date1 - date2;
+}
+
+
 exports.handler = async (event, context, callback) => {
 
     let responseCode = 200;
@@ -37,7 +45,7 @@ exports.handler = async (event, context, callback) => {
         // console.log(' -- t7 - DBG -- query:' + JSON.stringify(query) );
         const event = await db.collection(DB_COLLECTION).findOne(query);
         await client.close();
-        if ( event ) {
+        if (event) {
             responseBody.event = event;
         } else {
             responseCode = 404;
@@ -48,15 +56,16 @@ exports.handler = async (event, context, callback) => {
         const query = { year: parseInt(year) };
         // console.log(' -- t7 - DBG -- query:' + JSON.stringify(query) );
         const projection = { name: 1, dateStrg: 1 };
-        const events = await db.collection(DB_COLLECTION).find(query).project(projection).toArray();
+        let events = await db.collection(DB_COLLECTION).find(query).project(projection).toArray();
         await client.close();
-        if ( events && events.length > 0 ) {
+        events.sort(compareEvents);
+        if (events && events.length > 0) {
             responseBody.events = events;
         } else {
             responseCode = 404;
         }
     } else {
-        responseBody.years = [2017, 2019];
+        responseBody.years = [2017, 2018, 2019];
     }
 
     var response = {
